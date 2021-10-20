d1e9052771f036450a86c52944ada72397521138
rsadikur66 <rsadikur74@gmail.com>
2021-10-20 19:13:07 +0600

    from home commit 20oct2021

--- Ecommerce_BD_DAL/Repository/Query/Setup/CategorySetupDAL.cs
+++ Ecommerce_BD_DAL/Repository/Query/Setup/CategorySetupDAL.cs
@@ -16,12 +16,12 @@ namespace Ecommerce_BD_DAL.Repository.Query.Setup
         }
         public bool InsertData(string lang1, string lang2)
         {
-            Command($@"INSERT INTO CATEGORIES (T_LANG1_NAME, T_LANG2_NAME) VALUES ('{lang1}','{lang2}')");
+            Command($@"INSERT INTO CATEGORIES (T_LANG1_NAME, T_LANG2_NAME) VALUES (N'{lang1}','{lang2}')");
             return true;
         }
         public bool UpdateData(CategoryModel catMod)
         {
-            Command($@"UPDATE CATEGORIES SET  T_LANG1_NAME='{catMod.T_LANG1_NAME}',T_LANG2_NAME='{catMod.T_LANG2_NAME}' WHERE CATEGORY_ID = {catMod.CATEGORY_ID};");
+            Command($@"UPDATE CATEGORIES SET  T_LANG1_NAME=N'{catMod.T_LANG1_NAME}',T_LANG2_NAME='{catMod.T_LANG2_NAME}' WHERE CATEGORY_ID = {catMod.CATEGORY_ID};");
             return true;
         }
         public bool DeleteCategoryRecord(int catid)
