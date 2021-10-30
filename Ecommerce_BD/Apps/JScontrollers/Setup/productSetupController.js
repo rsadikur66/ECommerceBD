a86e0d35b5014d59b1abff095b7b324ac4212ef5
rsadikur66 <rsadikur74@gmail.com>
2021-10-31 01:35:43 +0600

    IMAGE PREVIEW ON PRODUCT SETUP

--- Ecommerce_BD/Apps/JScontrollers/Setup/productSetupController.js
+++ Ecommerce_BD/Apps/JScontrollers/Setup/productSetupController.js
@@ -1,4 +1,5 @@
-﻿
+﻿/*const { forEach } = require("angular");*/
+
 app.controller('productSetupController', ["$scope", "$window", "$location", "$filter", "$timeout", "productSetupService", "categorySetupService","Upload", function ($scope, $window, $location, $filter, $timeout, productSetupService, categorySetupService, Upload) {
     $scope.obj = {};
     $scope.obj.ambulance = [];
@@ -17,30 +18,75 @@ app.controller('productSetupController', ["$scope", "$window", "$location", "$fi
             $scope.obj.productsList = JSON.parse(data);
         })
     }
+    //3rd try
 
+    $(function () {
+        // Multiple images preview in browser
+        var imagesPreview = function (input, placeToInsertImagePreview) {
+            if (input.files) {
+                var filesAmount = input.files.length;
+                for (i = 0; i < filesAmount; i++) {
+                    var reader = new FileReader();
+                    reader.onload = function (event) {
+                        debugger;
+                        $($.parseHTML('<img>')).attr('src', event.target.result).appendTo(placeToInsertImagePreview);
+                    }
+                    reader.readAsDataURL(input.files[i]);
+                }
+            }
+        };
+
+        $('#gallery-photo-add').on('change', function () {
+            imagesPreview(this, 'div.gallery');
+        });
+    });
+
+
+
+    ///3rd try
+    $scope.preview_image = function (files) {
+        for (var i = 0; i < files.length; i++) {
+            $scope.indx = i;
+            var reader = new FileReader();
+            reader.onload = function () {
+                var output = document.getElementById('output_image');
+                $($.parseHTML('<img>')).attr('src', reader.result).appendTo(output);
+               // output.src = reader.result;
+            }
+
+            reader.readAsDataURL(files[i]);
+        }
+        
+    }
     //Upload
     $scope.UploadFiles = function (files) {
         $scope.SelectedFiles = files;
-        if ($scope.SelectedFiles && $scope.SelectedFiles.length) {
-            Upload.upload({
-                url: '/ProductSetup/PicUpload/',
-                data: {
-                    files: $scope.SelectedFiles
-                }
-            }).then(function (response) {
-                $timeout(function () {
-                    $scope.Result = response.data;
-                });
-            }, function (response) {
-                if (response.status > 0) {
-                    var errorMsg = response.status + ': ' + response.data;
-                    alert(errorMsg);
-                }
-            }, function (evt) {
-                var element = angular.element(document.querySelector('#dvProgress'));
-                $scope.Progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-                element.html('<div style="width: ' + $scope.Progress + '%">' + $scope.Progress + '%</div>');
-            });
+        
+        if ($scope.SelectedFiles && $scope.SelectedFiles.type == "image/jpeg") {
+            for (var i = 0; i < $scope.SelectedFiles; i++) {
+                /*document.getElementById("#imagePreview").innerHTML = "<div>This is inside my element with id=" + $scope.SelectedFiles+" </div>"*/
+                console.log($scope.SelectedFiles);
+            }
+           
+            //Upload.upload({
+            //    url: '/ProductSetup/PicUpload/',
+            //    data: {
+            //        files: $scope.SelectedFiles
+            //    }
+            //}).then(function (response) {
+            //    $timeout(function () {
+            //        $scope.Result = response.data;
+            //    });
+            //}, function (response) {
+            //    if (response.status > 0) {
+            //        var errorMsg = response.status + ': ' + response.data;
+            //        alert(errorMsg);
+            //    }
+            //}, function (evt) {
+            //    var element = angular.element(document.querySelector('#dvProgress'));
+            //    $scope.Progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+            //    element.html('<div style="width: ' + $scope.Progress + '%">' + $scope.Progress + '%</div>');
+            //});
         }
     };
 }
