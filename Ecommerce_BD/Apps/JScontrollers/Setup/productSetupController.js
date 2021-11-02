e3610bb56eaab4756a4b89dfd98295f1f5a89b5d
AdministratorOffice <Administrator@SADIK_PC>
2021-11-02 18:08:58 +0600

    upload multiple image with close button but not finish

--- Ecommerce_BD/Apps/JScontrollers/Setup/productSetupController.js
+++ Ecommerce_BD/Apps/JScontrollers/Setup/productSetupController.js
@@ -3,6 +3,7 @@
 app.controller('productSetupController', ["$scope", "$window", "$location", "$filter", "$timeout", "productSetupService", "categorySetupService","Upload", function ($scope, $window, $location, $filter, $timeout, productSetupService, categorySetupService, Upload) {
     $scope.obj = {};
     $scope.obj.ambulance = [];
+    $scope.files = [];
     getCategoriesData();   
     getProductListData();
     function getCategoriesData() {
@@ -19,6 +20,10 @@ app.controller('productSetupController', ["$scope", "$window", "$location", "$fi
         })
     }
     //3rd try
+    $scope.hide = function () {
+        document.getElementById("image1").style.display = "none";
+        document.getElementById("close").style.display = "none";
+    }
 
     $(function () {
         // Multiple images preview in browser
@@ -58,6 +63,23 @@ app.controller('productSetupController', ["$scope", "$window", "$location", "$fi
         }
         
     }
+    $scope.$on("fileSelected", function (event, args) {
+        var item = args;
+        $scope.files.push(item);
+        var reader = new FileReader();
+
+        reader.addEventListener("load", function () {
+            $scope.$apply(function () {
+                item.src = reader.result;
+            });
+        }, false);
+
+        if (item.file) {
+            reader.readAsDataURL(item.file);
+        }
+        //alert('function call.....');
+    });
+
     //Upload
     $scope.UploadFiles = function (files) {
         $scope.SelectedFiles = files;
@@ -92,7 +114,23 @@ app.controller('productSetupController', ["$scope", "$window", "$location", "$fi
 }
 ]);
 
-
+app.directive('fileUpload', function () {
+    return {
+        scope: true, //create a new scope
+        link: function (scope, el, attrs) {
+            el.bind('change', function (event) {
+                var files = event.target.files;
+                //iterate files since 'multiple' may be specified on the element
+                for (var i = 0; i < files.length; i++) {
+                    //emit event upward
+                    scope.$emit("fileSelected", {
+                        file: files[i]
+                    });
+                }
+            });
+        }
+    };
+});
 app.filter('propsFilter', function () {
     return function (items, props) {
         var out = [];
